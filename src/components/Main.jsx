afca27e77912a0a9ebee794d10fe46553d0fdbde
Hariharasudhan M <harimhs028@gmail.com>
2024-12-11 23:00:50 +0530

    form added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 
 const Main = () => {
+  const ing = ["chicken", "spice", "curry"];
+  const ingMap= ing.map((data)=>{
+        return <li key={data}>{data}</li>
+    })
+  function submit(event){
+    event.preventDefault()
+    console.log("submitted!");
+    const formData= new FormData(event.currentTarget)
+    const newIng = formData.get("ingredient");
+    console.log(newIng);    
+  }
   return (
     <main>
-    <form>
+    <form onSubmit={submit}>
         <input 
         type='text'
         placeholder='e.g spices'
         className='input-tag'
+        name='ingredient'
         />
         <button>Add ingredient</button>
     </form>
+    {ingMap}
     </main>
   )
 }
--- src/components/Rough.jsx
+++ src/components/Rough.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const Rough = () => {
 
-    const ing = ["chicken", "spice", "curry"];
-    const ingMap= ing.map((data)=>{
-        return <li key={data}>{data}</li>
-    })
+    
 
   return (
     <>
