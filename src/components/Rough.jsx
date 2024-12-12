5eb6c4a1b9846bfaab3811d9e8a475f8fb3fdc15
Hariharasudhan M <harimhs028@gmail.com>
2024-12-12 17:57:53 +0530

    form added

--- src/components/Rough.jsx
+++ src/components/Rough.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
 const Rough = () => {
-
     
+    const [count, setCount] = React.useState(0);
 
-  return (
+    function increment(){
+        setCount((prev)=>prev+1)
+        setCount((prev)=>prev+1)
+        setCount((prev)=>prev+1)
+    }
+
+    function decrement(){
+        setCount((prev)=>prev-1)
+    }
+    
+   
+return (
     <>
-        {ingMap} 
+        <main className="container">
+            <h1>How many times will Bob say "state" in this section?</h1>
+            <div className="counter">
+                <button className="minus" aria-label="Decrease count" onClick={decrement}>–</button>
+                <h2 className="count">{count}</h2>
+                <button className="plus" aria-label="Increase count" onClick={increment}>+</button>
+            </div>
+        </main>
     </>
   )
 }
