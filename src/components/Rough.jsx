f60ad0df4ade7c750170497e794c1bd747a7c192
Hariharasudhan M <harimhs028@gmail.com>
2024-12-12 19:22:50 +0530

    main component

--- src/components/Rough.jsx
+++ src/components/Rough.jsx
@@ -2,28 +2,15 @@ import React from 'react'
 
 const Rough = () => {
     
-    const [count, setCount] = React.useState(0);
-
-    function increment(){
-        setCount((prev)=>prev+1)
-        setCount((prev)=>prev+1)
-        setCount((prev)=>prev+1)
-    }
-
-    function decrement(){
-        setCount((prev)=>prev-1)
-    }
+    const isGoingOut = false
     
+    let answer = isGoingOut ? "yes " : "no"
    
 return (
     <>
-        <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button className="minus" aria-label="Decrease count" onClick={decrement}>–</button>
-                <h2 className="count">{count}</h2>
-                <button className="plus" aria-label="Increase count" onClick={increment}>+</button>
-            </div>
+        <main>
+            <h1 className="title">Do I feel like going out tonight?</h1>
+            <button className="value">{answer}</button>
         </main>
     </>
   )
