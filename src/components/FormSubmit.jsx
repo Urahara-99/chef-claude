7dd37816586175071dc05d0126378c123bf8d0b8
Hariharasudhan M <harimhs028@gmail.com>
2024-12-13 22:49:46 +0530

    Form Submission

---
+++ src/components/FormSubmit.jsx
@@ -0,0 +1,33 @@
+function handleSubmit(event){
+    event.preventDefault()
+    console.log("Form submitted")
+    const Fdata= event.currentTarget
+    const formData= new FormData(Fdata)
+    const gmail= formData.get("email")
+    console.log(gmail);
+    
+  }
+  
+  const Rough=()=> {
+    return (
+      <section>
+        <h1>Signup form</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+          <br />
+          
+          <label htmlFor="password">Password:</label>
+          <input id="password" type="password" name="password" />
+          <br />
+          
+          <button>Submit</button>
+          
+        </form>
+      </section>
+    )
+  }
+  
+  export default Rough
+  
+  
--- src/components/Rough.jsx
+++ src/components/Rough.jsx
@@ -1,57 +1,22 @@
-import React from "react"
-import avatar from "../assets/user.png"
-import starFilled from "../assets/star-filled.png"
-import starEmpty from "../assets/star-empty.png"
+import React from 'react'
 
-export default function App() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (212) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: true
-    })
+export default function Rough() {
 
-    let star= contact.isFavorite ? starFilled : starEmpty;
-    let ariavar = contact.isFavorite ? "Remove from fav" : "Add to fav";
-    let imgChangeAlt= contact.isFavorite ? "filled star" : "empty star";
-
-    function toggleFavorite() {
-        setContact((prev=>{
-          return {...prev, isFavorite: !prev.isFavorite}
-        }))
+    const [myFavoriteThings, setmyFavouriteThings] = React.useState([])
+    const allFavoriteThings = ["💦🌹", "😺", "💡", "🔥🧤", "🟤🎁", 
+    "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+    const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
+  
+    function addFavoriteThing() {
+        setmyFavouriteThings(prev=>[...prev, allFavoriteThings[prev.length]])
     }
-
+    
     return (
-        <main>
-            <article className="card">
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={false}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={star}
-                            aria-pressed={contact.isFavorite}
-                            aria-label={ariavar}
-                            alt={imgChangeAlt}
-                            className="favorite"
-                        />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-
-            </article>
-        </main>
+      <main>
+        <button onClick={addFavoriteThing}>Add item</button>
+        <section aria-live="polite">
+          {thingsElements}
+        </section>
+      </main>
     )
-}
+  }
