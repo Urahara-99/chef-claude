f423b1eaa47b2e9ca9233159bd12739323fa0da5
Hariharasudhan M <harimhs028@gmail.com>
2024-12-19 19:36:09 +0530

    Organized AI response with Markdown

--- src/components/Receipe.jsx
+++ src/components/Receipe.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Markdown from 'react-markdown'
+
 
 const Receipe = (props) => {
   return (
     <section>  
     <div className='suggest-list'><br /><br />
     <h2>Chef Claude Recommends:</h2>
-    <section>{props.recipe}</section>
+    <Markdown>{props.recipe}</Markdown>
     </div> 
-
     </section>
   )
 }
