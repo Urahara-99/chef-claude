0159da1aa55ea7537b83fc8d744b64facf0b355e
Hariharasudhan M <harimhs028@gmail.com>
2024-12-19 20:04:39 +0530

    Finishing project

--- src/components/Receipe.jsx
+++ src/components/Receipe.jsx
@@ -4,12 +4,12 @@ import Markdown from 'react-markdown'
 
 const Receipe = (props) => {
   return (
-    <section>  
-    <div className='suggest-list'><br /><br />
-    <h2>Chef Claude Recommends:</h2>
-    <Markdown>{props.recipe}</Markdown>
-    </div> 
+    <div className='recipe-container'>
+    <section className='suggested-recipe-container' aria-live='polite'>  
+    <h2 className='heading-recipe'>Chef Claude Recommends:</h2>
+    <Markdown>{props.recipe}</Markdown> 
     </section>
+    </div>
   )
 }
 
